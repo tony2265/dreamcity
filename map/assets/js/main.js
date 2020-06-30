bef8264e36251e1d7c5c82b5de84f1f940011830
Tony Won <34482694+tony2265@users.noreply.github.com>
2020-07-01 00:37:35 +0800

    ouo

--- map/assets/js/main.js
+++ map/assets/js/main.js
@@ -334,3 +334,19 @@
 			});
 
 })(jQuery);
+
+
+// Timeline
+
+$(function(){
+	var inputs = $('.input');
+	var paras = $('.description-flex-container').find('p');
+	$(inputs).click(function(){
+		var t = $(this),
+				ind = t.index(),
+				matchedPara = $(paras).eq(ind);
+		
+		$(t).add(matchedPara).addClass('active');
+		$(inputs).not(t).add($(paras).not(matchedPara)).removeClass('active');
+	});
+});
