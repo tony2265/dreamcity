082fe0400c4b7dc99d3a4cdfba165eb24b360165
Tony Won <34482694+tony2265@users.noreply.github.com>
2020-08-27 13:38:46 +0800

    投稿

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -48,3 +48,16 @@
 			});
 
 })(jQuery);
+
+$(function(){
+	var inputs = $('.input');
+	var paras = $('.description-flex-container').find('p');
+	$(inputs).click(function(){
+		var t = $(this),
+				ind = t.index(),
+				matchedPara = $(paras).eq(ind);
+		
+		$(t).add(matchedPara).addClass('active');
+		$(inputs).not(t).add($(paras).not(matchedPara)).removeClass('active');
+	});
+});
